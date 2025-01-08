21b57f15cd0cc08070dd4bab46a612ca9cd071b0
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2025-01-08 09:44:18 -0800

    Create 24-1001_SpatialFeaturePlotBlend.R

---
+++ Visium_Analysis/scripts/Function_Scripts/24-1001_SpatialFeaturePlotBlend.R
@@ -0,0 +1,164 @@
+### Author: Matthew Aaron Loberg
+### Date: October 1, 2024
+### Script: 24-1001_SpatialFeaturePlotBend.R
+
+###### Goal: #####
+# Be able to map two features at once in some capacity for Visium ST objects
+
+##### Script Credit #####
+# Script credit goes to "george-hall-ucl"
+# It was taken from the following GitHub page: 
+# https://github.com/george-hall-ucl/SpatialFeaturePlotBlend/blob/main/SpatialFeaturePlotBlend.R
+
+##### Function Script #####
+SpatialFeaturePlotBlend <- function(object, features, combine = TRUE,
+                                    feature_1_alt_name = NULL,
+                                    feature_2_alt_name = NULL, assay = NULL,
+                                    bottom_left = "#000000",
+                                    bottom_right = "#FF0000",
+                                    top_left = "#00FF00",
+                                    top_right = "#FFFF00", ...)  {
+  
+  # Generate a grid of RGB color values given the requested corner colours.
+  gen_color_grid <- function(side_length, bottom_left, bottom_right,
+                             top_left, top_right) {
+    
+    grad_gen <- function(start, end, n = side_length) {
+      colfunc <- colorRampPalette(c(start, end))
+      return(colfunc(n))
+    }
+    
+    # x_y = "x to y"; "bl" = "bottom left", etc
+    bl_tl <- grad_gen(bottom_left, bottom_right)
+    br_tr <- grad_gen(top_left, top_right)
+    
+    l <- lapply(seq_len(length(bl_tl)),
+                function(i) {
+                  start <- bl_tl[i]
+                  end <- br_tr[i]
+                  new_grad <- grad_gen(start, end)
+                })
+    
+    return(t(matrix(unlist(l), ncol = side_length, nrow = side_length)))
+  }
+  
+  if (length(features) != 2) {
+    stop(paste(c("Incorrect number of features. ",
+                 "Requires two features, received ",
+                 length(features))))
+  }
+  
+  if (!is.null(assay)) {
+    DefaultAssay(object) <- assay
+  }
+  
+  blend_plot_theme <- theme(legend.position = "none",
+                            plot.title = element_text(hjust = 0.5))
+  
+  plot_list_outer <- list()
+  
+  object$image_id <- unlist(lapply(Images(object),
+                                   function(x) {
+                                     rep(x, nrow(object[[x]]@coordinates))
+                                   }))
+  
+  
+  for (i in Images(object)) {
+    plot_list <- lapply(features,
+                        function(feature) {
+                          max_color <- ifelse(feature == features[1],
+                                              bottom_right, top_left)
+                          SpatialFeaturePlot(object, feature,
+                                             images = i, ...) +
+                            scale_fill_gradient(low = bottom_left,
+                                                high = max_color) +
+                            ggtitle(feature) +
+                            blend_plot_theme
+                        })
+    
+    cells_obj_sub <- subset(object, image_id == i)
+    dat <- FetchData(cells_obj_sub, features)
+    side_length <- 100
+    col_grid <- gen_color_grid(side_length, bottom_left, bottom_right,
+                               top_left, top_right)
+    dat_norm <- apply(dat, 2,
+                      function(x) {
+                        round((side_length - 1) * x / max(x)) + 1
+                      })
+    colors <- sapply(seq_len(nrow(dat_norm)),
+                     function(x) {
+                       col_grid[dat_norm[x, 1], dat_norm[x, 2]]
+                     })
+    
+    new_md_column <- paste0(features[1], "_vs_", features[2])
+    cells_obj_sub[[new_md_column]] <- colors
+    names(colors) <- as.character(colors)
+    
+    plot_list[[3]] <- SpatialDimPlot(cells_obj_sub, new_md_column,
+                                     cols = colors, images = i, ...) +
+      ggtitle(paste0(features[1], "_", features[2])) +
+      blend_plot_theme
+    
+    legend_grid <- expand.grid(seq(from = min(dat[, features[1]]),
+                                   to = max(dat[, features[1]]),
+                                   length.out = side_length),
+                               seq(from = min(dat[, features[2]]),
+                                   to = max(dat[, features[2]]),
+                                   length.out = side_length))
+    colnames(legend_grid) <- features
+    legend_colors <- c(col_grid)
+    legend_grid$color <- legend_colors
+    names(legend_colors) <- legend_colors
+    
+    legend <- ggplot(legend_grid,
+                     aes(x = .data[[features[1]]], y = .data[[features[2]]],
+                         color = color)) +
+      geom_point(shape = 15, size = 1.9) +
+      scale_color_manual(values = legend_colors) +
+      coord_cartesian(expand = FALSE) +
+      theme(legend.position = "none", aspect.ratio = 1,
+            panel.background = element_blank(),
+            axis.text.x = element_text(angle = 45, hjust = 1)) +
+      xlab(ifelse(is.null(feature_1_alt_name),
+                  features[1], feature_1_alt_name)) +
+      ylab(ifelse(is.null(feature_2_alt_name),
+                  features[2], feature_2_alt_name))
+    
+    plot_list[[4]] <- wrap_plots(ggplot() + theme_void(), legend,
+                                 ggplot() + theme_void(), ncol = 1,
+                                 heights = c(0.2, 0.6, 0.2))
+    
+    plot_list_outer[[i]] <- plot_list
+  }
+  
+  if (combine == FALSE) {
+    return(plot_list_outer)
+  } else {
+    plot_list_outer <- lapply(plot_list_outer,
+                              function(p) {
+                                wrap_plots(p, nrow = 1,
+                                           widths = c(0.28, 0.28,
+                                                      0.28, 0.16))
+                              })
+    p <- wrap_plots(plot_list_outer, ncol = 1)
+    
+    return(p)
+  }
+}
+
+##### Licensing Information From GitHub #####
+# Copyright (C) 2024 University College London, licensed under GNU General
+# Public License v3.0.
+# 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
