409dcea3727b404948eda0a8ad1a96e27bbd1c96
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2025-01-15 10:40:01 -0600

    Update and rename Visium_Analysis/scripts/22-0913_Thy1_Processing_Raw_SCTransform.R to Visium_Analysis/scripts/Seurat_Preprocessing/Thy1_Processing_Raw_SCTransform.R

--- Visium_Analysis/scripts/Seurat_Preprocessing/Thy1_Processing_Raw_SCTransform.R
+++ Visium_Analysis/scripts/Seurat_Preprocessing/Thy1_Processing_Raw_SCTransform.R
@@ -1,7 +1,11 @@
 # Author: Matthew A. Loberg
 # Date: September 13th, 2022
-# Purpose: New Visium sequencing data just obtained from Vantage
+# Script: Thy1_Processing_Raw_SCTransform.R
+# Source Script Name: 22-0913_Thy1_Processing_Raw_SCTransform.R
+
+# Goal: 
 # Here, I will read the data into R studio and begin basic processing of the data
+# I will save a seurat object as a .RDS, which I will use for future analysis
 
 #### Chapter 1: Loading Packages ####
 # Load required packages
@@ -15,7 +19,7 @@ library(tidyverse)
 #### Chapter 2: Reading in Thy1 and looking at raw count data by violin and SpatialFeaturePlot ####
 
 # Load in Thy1 data
-data_dir <- 'Data_in_Use/2021_JHU_Data/Thy1' # Set directory to load from
+data_dir <- 'Data_in_Use/Thy1' # Set directory to load from
 Thy1 <- Seurat::Load10X_Spatial(data.dir = data_dir, slice = "slice1") # Load Thy1
 Thy1$orig.ident <- "Thy1"
 # Cleaning up
@@ -51,8 +55,8 @@ ggsave("outputs/Thy1_QC/22-0913_Thy1_Processing_Raw_SCTransform/22-0913_Raw_Coun
 # Cleaning up
 rm(plot1, plot2, test)
 
-# Save raw Thy1 as an RDS
-saveRDS(Thy1, "Data_in_Use/2021_JHU_Data/Thy1_Processed/22-0913_Thy1_Raw_PreProcessed.rds")
+# Save raw Thy1 Seurat Object as an RDS (prior to SCTransform - just contains raw count data)
+saveRDS(Thy1, "Data_in_Use/Thy1_Processed/22-0913_Thy1_Raw_PreProcessed.rds")
 
 #### Chapter 3: Data Transformation ####
 # I will perform data transformation with SCTransform
@@ -61,17 +65,22 @@ saveRDS(Thy1, "Data_in_Use/2021_JHU_Data/Thy1_Processed/22-0913_Thy1_Raw_PreProc
 # There are many discussions on whether return.only.var.genes should be set to TRUE/FALSE.
 # In the vignette from satijalab, they recommend FALSE for best performance. 
 # I need to do more reading to see how this affects addModuleScore and other commands
-Thy1 <- SCTransform(Thy1, assay = "Spatial", return.only.var.genes = FALSE, verbose = FALSE)
+Thy1 <- SCTransform(Thy1, 
+                    vst.flavor = "v2",
+                    assay = "Spatial", 
+                    return.only.var.genes = FALSE, 
+                    verbose = FALSE)
 
-# Save SCTransformed Thy1 as an RDS
-saveRDS(Thy1, "Data_in_Use/2021_JHU_Data/Thy1_Processed/22-0913_Thy1_SCTransformed_All_Genes.rds")
+# Save SCTransformed Thy1 Seurat Object as an RDS for future use
+saveRDS(Thy1, "Data_in_Use/Thy1_Processed/22-0913_Thy1_SCTransformed_All_Genes.rds")
+
+# Just testing spatial feature plot
+SpatialFeaturePlot(Thy1, features = "TGFB1")
+SpatialFeaturePlot(Thy1, features = "GREM1")
 
 # Cleaning up
 rm(Thy1)
 
-SpatialFeaturePlot(Thy1, features = "TGFB1")
-SpatialFeaturePlot(Thy1, features = "GREM1")
-
 # I will start subsequent analysis by loading the SCTransformed/processed version of Thy1
-# Can use this SCTransformed assay to addmodule score (note for Andres 24-0126)
+# Can use this SCTransformed assay to addmodule score, perform other analyses
 
