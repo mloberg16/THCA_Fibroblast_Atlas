dc81fbfd2a038dbfdd99f5005541561afcb597ec
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2025-01-28 10:55:29 -0600

    Create 22-1031_ReadData.R

---
+++ Integrated_scRNA-Seq_Analysis/scripts/Luo_etal_Analysis_Scripts/22-1031_ReadData.R
@@ -0,0 +1,23 @@
+### Author: Matthew Loberg
+### Script: 22-1031_ReadData.R
+### Goal: Read in scRNA-sequencing data from Luo at all and work on formatting it for analysis with Seurat and eventual integration
+
+## Data organization
+# Data is formatted as a raw count matrix for all samples: "thyroid_count_matrix.csv"
+# I will take this matrix and just save it as a .RDS here for future splitting into individual objects, etc.
+
+## Using the following tutorial for loading data
+# https://learn.gencore.bio.nyu.edu/single-cell-rnaseq/loading-your-own-data-in-seurat-reanalyze-a-different-dataset/
+# The biggest challenge is that the samples are merged in this data file
+# Luckily, the cell IDs are prefixed with sample IDs, so I can track individual cells to individual samples
+
+# Load packages
+library(tidyverse) # contains dplyr
+library(Matrix)
+
+# Load count table
+raw_counts <- read.table(file = "data_in_use/thyroid_count_matrix.csv", sep = ",", header = TRUE)
+
+# Save this file as an RDS for faster load times
+saveRDS(raw_counts, file = "data_in_use/thyroid_raw_count_matrix.rds")
+rm(raw_counts)
