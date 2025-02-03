72d8a102cd5f98bb8fa83ecaeb9ade5155b53713
mloberg16 <80286246+mloberg16@users.noreply.github.com>
2025-02-03 10:46:21 -0600

    Update Broad_Cell_Type_Proportions.R

--- Integrated_scRNA-Seq_Analysis/scripts/Atlas_Integration/Broad_Cell_Type_Proportions.R
+++ Integrated_scRNA-Seq_Analysis/scripts/Atlas_Integration/Broad_Cell_Type_Proportions.R
@@ -229,3 +229,52 @@ dir.create(savedir)
 ggsave(file.path(savedir, "RNA_snn_res.0.6_Final_Labels_by_Paper_Histo_V3.png"),
        plot,
        height = 5, width = 10.5, dpi = 600)
+
+
+### Session Info
+sessionInfo()
+
+# > sessionInfo()
+# R version 4.3.1 (2023-06-16 ucrt)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 11 x64 (build 22631)
+#
+# Matrix products: default
+#
+#
+# locale:
+#   [1] LC_COLLATE=English_United States.utf8  LC_CTYPE=English_United States.utf8    LC_MONETARY=English_United States.utf8
+# [4] LC_NUMERIC=C                           LC_TIME=English_United States.utf8
+#
+# time zone: America/Chicago
+# tzcode source: internal
+#
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base
+#
+# other attached packages:
+#   [1] lubridate_1.9.3    forcats_1.0.0      stringr_1.5.1      dplyr_1.1.4        purrr_1.0.2        readr_2.1.4        tidyr_1.3.0
+# [8] tibble_3.2.1       ggplot2_3.5.0      tidyverse_2.0.0    SeuratObject_4.1.4 Seurat_4.4.0
+#
+# loaded via a namespace (and not attached):
+#   [1] RColorBrewer_1.1-3     rstudioapi_0.15.0      jsonlite_1.8.7         magrittr_2.0.3         spatstat.utils_3.1-2
+# [6] ggbeeswarm_0.7.2       vctrs_0.6.4            ROCR_1.0-11            spatstat.explore_3.2-5 htmltools_0.5.7
+# [11] sctransform_0.4.1      parallelly_1.36.0      KernSmooth_2.23-21     htmlwidgets_1.6.2      ica_1.0-3
+# [16] plyr_1.8.9             plotly_4.10.3          zoo_1.8-12             igraph_2.0.3           mime_0.12
+# [21] lifecycle_1.0.4        pkgconfig_2.0.3        Matrix_1.6-1           R6_2.5.1               fastmap_1.1.1
+# [26] fitdistrplus_1.1-11    future_1.33.0          shiny_1.8.0            digest_0.6.33          colorspace_2.1-0
+# [31] patchwork_1.2.0        tensor_1.5             irlba_2.3.5.1          progressr_0.14.0       timechange_0.2.0
+# [36] fansi_1.0.5            spatstat.sparse_3.0-3  httr_1.4.7             polyclip_1.10-6        abind_1.4-5
+# [41] compiler_4.3.1         withr_2.5.2            MASS_7.3-60            tools_4.3.1            vipor_0.4.5
+# [46] lmtest_0.9-40          beeswarm_0.4.0         httpuv_1.6.12          future.apply_1.11.0    goftest_1.2-3
+# [51] glue_1.6.2             nlme_3.1-162           promises_1.2.1         grid_4.3.1             Rtsne_0.16
+# [56] cluster_2.1.4          reshape2_1.4.4         generics_0.1.3         gtable_0.3.4           spatstat.data_3.0-3
+# [61] tzdb_0.4.0             hms_1.1.3              data.table_1.14.8      sp_2.1-1               utf8_1.2.4
+# [66] spatstat.geom_3.2-7    RcppAnnoy_0.0.21       ggrepel_0.9.4          RANN_2.6.1             pillar_1.9.0
+# [71] later_1.3.1            splines_4.3.1          lattice_0.21-8         survival_3.5-5         deldir_1.0-9
+# [76] tidyselect_1.2.0       miniUI_0.1.1.1         pbapply_1.7-2          gridExtra_2.3          scattermore_1.2
+# [81] matrixStats_1.1.0      stringi_1.8.1          lazyeval_0.2.2         codetools_0.2-19       cli_3.6.1
+# [86] uwot_0.1.16            xtable_1.8-4           reticulate_1.34.0      munsell_0.5.0          Rcpp_1.0.11
+# [91] globals_0.16.2         spatstat.random_3.2-1  png_0.1-8              ggrastr_1.0.2          parallel_4.3.1
+# [96] ellipsis_0.3.2         listenv_0.9.0          viridisLite_0.4.2      scales_1.3.0           ggridges_0.5.4
+# [101] leiden_0.4.3.1         rlang_1.1.2            cowplot_1.1.1
